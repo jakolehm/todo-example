b8d9d30fcd28f8f3627870d4801bfa4e1ddc15ad
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-05-11 12:59:50 -0400

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
+docker login -u "${QUAY_USERNAME}" -p "${QUAY_PASSWORD}" quay.io
+
+kontena stack build
 kontena stack install --name todo kontena.yml || \
     kontena stack upgrade todo kontena.yml
